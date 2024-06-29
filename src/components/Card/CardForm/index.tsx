45b2072e451451ea0c3120630ef5a494fa2af3d7
kauatwn <kaualopescontato@gmail.com>
2024-06-29 20:31:35 -0300

    Adicionando useState para campos em CardForm

--- src/components/Card/CardForm/index.tsx
+++ src/components/Card/CardForm/index.tsx
@@ -1,12 +1,40 @@
 import "./CardForm.css";
 import TextField from "../../TextField";
+import DropdownField from "../../DropdownField";
+import Button from "../../Button";
+import { useState } from "react";
 
 function CardForm() {
+  const [name, setName] = useState<string>("");
+  const [image, setImage] = useState<string>("");
+  const [position, setPosition] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
-    <form action="" className="card-form">
-      <TextField label="Nome" placeholder="Digite seu nome" />
-      <TextField label="Cargo" placeholder="Digite seu cargo" />
-      <TextField label="Imagem" placeholder="Digite o endereço da imagem" />
+    <form className="card-form" onSubmit={handleSubmit}>
+      <TextField
+        label="Nome"
+        placeholder="Digite seu nome"
+        value={name}
+        setValue={(value) => setName(value)}
+      />
+      <TextField
+        label="Cargo"
+        placeholder="Digite seu cargo"
+        value={position}
+        setValue={(value) => setPosition(value)}
+      />
+      <TextField
+        label="Imagem"
+        placeholder="Digite o endereço da imagem"
+        value={image}
+        setValue={(value) => setImage(value)}
+      />
+      <DropdownField label="Time" items={["Programação"]} />
+      <Button text="Criar card" />
     </form>
   );
 }
