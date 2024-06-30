32b1a28ac4d3a514dea0d1ac8c74e304e50e99cf
kauatwn <kaualopescontato@gmail.com>
2024-06-30 11:38:42 -0300

    Ajuste no DropdownField para delegar o useState ao CardForm

--- src/components/DropdownField/index.tsx
+++ src/components/DropdownField/index.tsx
@@ -1,15 +1,22 @@
+import { ChangeEvent } from "react";
 import "./DropdownField.css";
 
 interface DropdownFieldProps {
   label: string;
   items: string[];
+  value: string;
+  setValue: (value: string) => void;
 }
 
-function DropdownField({ label, items }: DropdownFieldProps) {
+function DropdownField({ label, items, value, setValue }: DropdownFieldProps) {
+  const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setValue(e.target.value);
+  };
+
   return (
     <div className="dropdown-field">
       <label htmlFor="">{label}</label>
-      <select name="" id="">
+      <select name="" id="" value={value} onChange={handleSelectChange}>
         {items.map((item) => {
           return <option key={item}>{item}</option>;
         })}
