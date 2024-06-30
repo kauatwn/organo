d020a16167dd428116c25ea49a10886db6f23b4c
kauatwn <kaualopescontato@gmail.com>
2024-06-30 15:17:47 -0300

    Atualiza FormField para passar teamList e onCollaboratorSubmit para CardForm

--- src/components/FormField/index.tsx
+++ src/components/FormField/index.tsx
@@ -1,11 +1,32 @@
-import { CardForm } from "../Card";
 import "./FormField.css";
+import { useState } from "react";
+import { CardForm } from "../Card";
+import { ICollaborator } from "../../interfaces";
+
+const teams: string[] = [
+  "Programação",
+  "Front-end",
+  "Data Science",
+  "DevOps",
+  "UX e Design",
+  "Mobile",
+  "Inovação e Gestão",
+];
 
 function FormField() {
+  const [collaborators, setCollaborators] = useState<ICollaborator[]>([]);
+
+  const handleCollaboratorSubmit = (collaborator: ICollaborator) => {
+    setCollaborators([...collaborators, collaborator]);
+  };
+
   return (
     <section className="form-field">
       <h2>Preencha os dados para criar o card do colaborador</h2>
-      <CardForm />
+      <CardForm
+        teamList={teams}
+        onCollaboratorSubmit={handleCollaboratorSubmit}
+      />
     </section>
   );
 }
