e44575cda11149255280a34ac5be6c50b76bf6a0
kauatwn <kaualopescontato@gmail.com>
2024-07-01 14:31:29 -0300

    Renomeando e refatorando FormField

---
+++ src/components/CollaboratorForm/index.tsx
@@ -0,0 +1,64 @@
+import "./CollaboratorForm.css";
+import { ICollaborator, ITeam } from "../../interfaces";
+import { useState } from "react";
+import TextField from "../TextField";
+import DropdownField from "../DropdownField";
+import Button from "../Button";
+
+interface ICollaboratorFormProps {
+  teams: ITeam[];
+  onCollaboratorSubmit: (collaborator: ICollaborator) => void;
+}
+
+function CollaboratorForm({
+  teams,
+  onCollaboratorSubmit,
+}: ICollaboratorFormProps) {
+  const [name, setName] = useState<string>("");
+  const [image, setImage] = useState<string>("");
+  const [position, setPosition] = useState<string>("");
+  const [team, setTeam] = useState<string>("");
+
+  const handleCollaboratorSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onCollaboratorSubmit({ name, image, position, team });
+  };
+
+  return (
+    <section className="collaborator-form-section">
+      <h2>Preencha os dados para criar o card do colaborador</h2>
+      <form className="collaborator-form" onSubmit={handleCollaboratorSubmit}>
+        <TextField
+          label="Nome"
+          placeholder="Digite seu nome"
+          selectedValue={name}
+          setValue={(value) => setName(value)}
+          isRequired={true}
+        />
+        <TextField
+          label="Cargo"
+          placeholder="Digite seu cargo"
+          selectedValue={position}
+          setValue={(value) => setPosition(value)}
+          isRequired={true}
+        />
+        <TextField
+          label="Imagem"
+          placeholder="Digite o endereço da imagem"
+          selectedValue={image}
+          setValue={(value) => setImage(value)}
+        />
+        <DropdownField
+          label="Time"
+          items={teams.map((team) => team.name)}
+          selectedValue={team}
+          setValue={(value) => setTeam(value)}
+          isRequired={true}
+        />
+        <Button text="Criar card" />
+      </form>
+    </section>
+  );
+}
+
+export default CollaboratorForm;
